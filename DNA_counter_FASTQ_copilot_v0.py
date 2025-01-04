2134c96ba7f2aac68184d7be2c4e77a557744e39
DDomogala3 <ddd38@nau.edu>
2025-01-03 19:11:51 -0600

    Changed counter to percentage

--- DNA_counter_FASTQ_copilot_v0.py
+++ DNA_counter_FASTQ_copilot_v0.py
@@ -6,19 +6,28 @@ def DNA_count(text):
     T = 0
     G = 0
     C = 0
+    SUM = 0
     for i in text:
         if i == "A":
             A += 1
+            SUM += 1
         elif i == "T":
             T += 1
+            SUM += 1
         elif i == "G":
             G += 1
+            SUM += 1
         elif i == "C":
             C += 1
+            SUM += 1
         else:
             pass
-    return A, T, G, C
-
+    A_percent = round(A/SUM * 100, 2)
+    T_percent = round(T/SUM * 100, 2)
+    G_percent = round(G/SUM * 100, 2)
+    C_percent = round(C/SUM * 100, 2)
+#    return A, T, G, C, SUM, A_percent, T_percent, G_percent, C_percent
+    return A_percent, T_percent, G_percent, C_percent
 def open_file():
     file_path = filedialog.askopenfilename(filetypes=[("FASTQ files", "*.fastq"), ("All files", "*.*")])
     if file_path:
@@ -38,7 +47,7 @@ def open_file():
             label.config(text="A: %s T: %s G: %s C: %s" % DNA_count(text))
 
 window = tk.Tk()
-window.title("DNA Base Counter")
+window.title("DNA Base Percentage Counter")
 
 button_open = tk.Button(window, text="Open FASTQ File", command=open_file)
 button_open.pack()
